ded3c0e2445c04218bba1bd9c4cd5e92864acb43
서태원 <stainless@seotaewon-ui-MacBookPro.local>
2023-04-05 12:59:52 +0900

    SPA 라우츠 링크와 하이라이팅(active)

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,3 +1,16 @@
-<heaver>
+<script>
+    import { link } from "svelte-spa-router";
+    import active from "svelte-spa-router/active";
+</script>
+
+<header>
     HEADER!
-</heaver>
+    <a use:link use:active  href="/"> Home </a>
+    <a use:link use:active ={'/about'} href="/about">About</a>
+</header>
+
+<style lang="scss">
+    header :global(a.active) {
+        color: red;
+    }
+</style>
