3787270fdc3a7eb0a7e71889efe553453d94977e
서태원 <stainless@seotaewon-ui-MacBookPro.local>
2023-04-05 15:07:47 +0900

    Footer 컴포넌트 작성

---
+++ src/components/Footer.svelte
@@ -0,0 +1,27 @@
+<footer>
+    <div>
+        <a href="https://github.com/starbuckschair/svelte-movie-app">
+            GitHub Repo.
+        </a>
+    </div>
+    <div>
+        <a href="https://google.com">
+            @{new Date().getFullYear()}
+            Cain
+        </a>
+    </div>
+</footer>
+
+<style lang="scss">
+    footer {
+        padding: 70px;
+        text-align: center;
+        a {
+            color: $color--white-10;
+            text-decoration: none;
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+</style>
