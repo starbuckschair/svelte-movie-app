ec4fee43aca6a326f7d3f7001034fca5986e90ce
서태원 <stainless@seotaewon-ui-MacBookPro.local>
2023-04-05 17:03:09 +0900

    Search 컴포넌트 작성

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,9 +1,12 @@
 <script>
     import Headline from "~/components/Headline.svelte";
+    import Search from "~/components/Search.svelte";
+
 </script>
 
 <div class="container">
     <Headline />
+    <Search />
 </div>
 
 <style lang="scss">
