9d5864edab54bd21ca13518c17e89ffa0c545108
서태원 <stainless@seotaewon-ui-MacBookPro.local>
2023-04-06 15:41:08 +0900

    응답 에러 출력과 movie.js 리펙토링

--- src/components/MovieList.svelte
+++ src/components/MovieList.svelte
@@ -1,16 +1,19 @@
 <script>
-    import { movies, loading } from "~/store/movie";
+    import { movies, loading, message } from "~/store/movie";
     import MovieCard from "~/components/MovieCard.svelte";
     import Loader from "~/components/Loader.svelte";
 </script>
 
-<div class="movie-list">
+<div class:no-result={!$movies.length} class="movie-list">
     {#if $loading}
         <Loader />
     {/if}
+    <div class="message">
+        {$message}
+    </div>
     <div class="movies">
         {#each $movies as movie (movie.imdbID)}
-            <MovieCard {movie}/>
+            <MovieCard {movie} />
         {/each}
     </div>
 </div>
@@ -20,8 +23,16 @@
         margin-top: 30px;
         padding: 10px;
         background-color: $color--area;
-        border-radius:4px;
+        border-radius: 4px;
         text-align: center;
+        &.no-result {
+            padding: 70px 0;
+        }
+        .message {
+            color: $color--primary;
+            font-size: 20px;
+            text-align: center;
+        }
         .movies {
             display: flex;
             flex-wrap: wrap;
--- src/store/movie.js
+++ src/store/movie.js
@@ -5,26 +5,40 @@ import { writable, get } from 'svelte/store'
 export const movies = writable([])
 export const loading = writable(false)
 export const theMovie = writable({})
+export const message = writable('Search for th movie title!')
 
 export async function searchMovies(payload) {
     if(get(loading)) return 
     loading.set(true)
 
-    const { title, type, year, number } = payload
-    const OMDB_API_KEY = '31e062b9'
+    let total = 0
 
-    const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}`)
-    console.log(res)
+    try {
+        const res = await _fetchMovie({
+            ...payload,
+            page: 1
+        })
+        const { Search, totalResults } = res.data
+        movies.set(Search)
+        total = totalResults 
+    } catch (meg) {
+        movies.set([])
+        message.set(meg)
+        loading.set(false)
+        return
+    }
 
-    const { Search, totalResults } = res.data
-    movies.set(Search)
 
-    const pageLength = Math.ceil(totalResults / 10)
+
+    const pageLength = Math.ceil(total / 10)
 
     if (pageLength > 1) {
         for (let page = 2; page <= pageLength; page += 1) {
             if (page > (number / 10)) break
-            const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+            const res = await _fetchMovie({
+                ...payload,
+                page
+            })
             const { Search } = res.data
             movies.update($movies => _unionBy($movies, Search, 'imdbID'))
         }
@@ -36,9 +50,33 @@ export async function searchMovieWithId(id){
     if(get(loading)) return
     loading.set(true)
 
-    const OMDB_API_KEY = '31e062b9'
-    const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}&plot=full`)
+    const res = await _fetchMovie({
+        id
+    })
     console.log(res)
     theMovie.set(res.data)
     loading.set(false)
 } 
+
+function _fetchMovie(payload) {
+    const {title, type, year, page, id} = payload
+    const OMDB_API_KEY = '31e062b9'
+
+    const url = id 
+        ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}&plot=full` 
+        : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}`
+
+    return new Promise(async (resolve, reject)=>{
+        try {
+            const res = await axios.get(url)
+            console.log(res.data)
+            if(res.data.Error){
+                reject(res.data.Error)
+            }
+            resolve(res)
+        } catch (error){
+            console.log(error.response.status)
+            reject(error.message)
+        }
+    })
+}
